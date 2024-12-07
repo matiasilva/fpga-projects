df8515832a0e11ffea4e0661320f0133bb5908f9
Matias Silva <git@matiasilva.com>
2024-12-07 23:31:33 +0000

    Update FIFO

--- tangland/hdl/lcd_basic/fifo.v
+++ tangland/hdl/lcd_basic/fifo.v
@@ -18,6 +18,37 @@ module fifo #(
    output wire [WORD_WIDTH-1:0] rd_data,
 );
 
-   reg [$clog2(WORD_WIDTH)-1:0] rd_ptr, wr_ptr;
+   localparam PTRLEN $clog2(DEPTH) // need n+1 bits
+
+   reg [PTRLEN:0] rd_ptr, wr_ptr;
+   reg [WORD_WIDTH-1:0] mem [0:DEPTH-1];
+
+   wire [PTRLEN:0] level = wr_ptr - rd_ptr;
+   wire empty = (level == 0);
+   wire full = (level == {1'b1, {(PTRLEN){1'b0}}});
+
+   always @(posedge clk or negedge rst) begin
+      if (~rst) begin
+         rd_ptr <= 0;
+         wr_ptr <= 0;
+      end else begin
+         if (rd_ready && rd_valid) begin
+            rd_ptr <= rd_ptr + 1;
+            if (full) wr_ptr <= 0;
+         end
+         if (wr_ready && wr_valid) begin
+            mem[wr_ptr] <= wr_data;
+            wr_ptr <= wr_ptr + 1;
+         end
+      
+      
+      end
+      
+   
+   end
+
+   assign rd_valid = !empty;
+   assign wr_ready = !full;
+   assign rd_data = mem[rd_ptr];
 
 endmodule
